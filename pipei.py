3a5a4d61cde30b679abae0c9da43d783d4ec708c
yiqiong <2367512961@qq.com>
2020-03-15 13:29:09 +0800

    update pipei.py

--- pipei.py
+++ pipei.py
@@ -4,3 +4,27 @@ Created on Tue Apr  9 18:39:07 2019
 
 @author: yiqiong
 """
+left=['{','[','(','<']
+right=['}',']',')','>']
+stack=[]
+string=input()
+flag=0
+for char in string:
+    if char in left:
+        stack.append(char)
+    elif char in right:
+        if len(stack)==0:
+            flag=1
+            break
+    if left.index(stack[len(stack)-1])==right.index(char):
+        del stack[len(stack)-1]
+    else:
+        flag=1
+        break
+if flag==0:
+    if len(stack)==0:
+        print('yes')
+    else:
+        print('no')
+else:
+    print('no')
